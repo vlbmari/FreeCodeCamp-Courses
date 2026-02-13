418707b611c4cb180b1be21878e54def3ca89992
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-02-13 15:46:54 -0300

    JS - Variables and Strings

---
+++ Responsive Web Design Certification/JS-Workshop/build-a-sentence-maker.js
@@ -0,0 +1,19 @@
+let adjective = "friendly";
+let noun = "dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "giant";
+let noun2 = "roasted knights";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The "+ noun +" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was " +verb+ ".";
+console.log("First story: " + firstStory);
+
+adjective = "brave"; 
+noun = "knight";        
+verb = "hungry";      
+place = "castle";       
+adjective2 = "funny";   
+noun2 = "magic beans";   
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The "+ noun +" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was " +verb+ ".";
+console.log("Second story: " + secondStory);
