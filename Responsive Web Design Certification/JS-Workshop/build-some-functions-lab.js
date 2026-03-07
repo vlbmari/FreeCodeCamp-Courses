47f6c3b9777801026650517ab3db626cd31e2c5e
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-03-07 14:40:56 -0300

    JS - Functions

--- Responsive Web Design Certification/JS-Workshop/build-some-functions-lab.js
+++ Responsive Web Design Certification/JS-Workshop/build-some-functions-lab.js
@@ -6,3 +6,13 @@ function booWho(x){
     return false
   }
 }
+
+//Build an Email Masker
+function maskEmail(email){
+  const indexEmail = email.indexOf('@');
+  email = email.replace(email.slice(1, indexEmail - 1), "*".repeat(indexEmail - 2));
+  return email;
+}
+
+const email = 'apple.pie@example.com';
+console.log(maskEmail(email));
