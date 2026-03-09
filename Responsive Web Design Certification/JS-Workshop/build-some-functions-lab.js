bb73f6e9b2bdabe14ca8dcadf2e30649481e3d63
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-03-09 08:40:38 -0300

    JS - Functions Lab

--- Responsive Web Design Certification/JS-Workshop/build-some-functions-lab.js
+++ Responsive Web Design Certification/JS-Workshop/build-some-functions-lab.js
@@ -21,3 +21,31 @@ console.log(maskEmail(email));
 function convertCtoF(tempCelsius){
   return tempCelsius * (9/5) + 32;
 }
+
+//Build a Card Counting Assistant
+let count = 0;
+
+function cardCounter(card){
+  const lowCard = [2,3,4,5,6];
+  const highCard = [10,"J","Q","K","A"];
+
+  if(lowCard.includes(card)){
+    count++
+  }else if (highCard.includes(card)){
+    count--;
+  }
+  let decision = count > 0 ? "Bet": "Hold";
+  return `${count} ${decision}`;
+}
+
+count = 0;
+cardCounter(10); cardCounter("J"); cardCounter("Q"); cardCounter("K");
+console.log(cardCounter("A")); 
+
+count = 0;
+cardCounter(3); cardCounter(7); cardCounter("Q"); cardCounter(8);
+console.log(cardCounter("A")); 
+
+count = 0;
+cardCounter(2); cardCounter("J"); cardCounter(9); cardCounter(2);
+console.log(cardCounter(7)); 
