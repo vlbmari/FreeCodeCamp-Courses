5c5cde710ca8e10e36ebb004f73ae628e7ac79b4
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-03-13 12:42:43 -0300

    JS - Organizing Folders

--- Responsive Web Design Certification/JS-Workshop/lab-arrays.js
+++ Responsive Web Design Certification/JS-Workshop/lab-arrays.js
@@ -62,3 +62,25 @@ console.log(removeLastLunch(lunches))
 console.log(getRandomLunch(lunches))
 console.log(showLunchMenu(lunches))
 
+//Build a Golf Score Translator
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par,strokes){
+  if(strokes == 1){
+    return names[0];
+  }else if(strokes <= (par - 2)){
+    return names[1];
+  }else if(strokes == (par - 1)){
+    return names[2];
+  }else if(strokes == par){
+    return names[3];
+  }else if(strokes == (par + 1)){
+    return names[4];
+  }else if(strokes == (par + 2)){
+    return names[5];
+  }else if(strokes >= (par + 3)){
+    return names[6];
+  }
+}
+
+console.log(golfScore(2,4))
