e26cadd595b4aa9ab71e2ffd189548414d9cad19
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-03-26 18:03:35 -0300

    JS - Loops Lab

--- Responsive Web Design Certification/JS-Workshop/lab-loops.js
+++ Responsive Web Design Certification/JS-Workshop/lab-loops.js
@@ -10,3 +10,17 @@ const findLongestWordLength = (sentence) => {
   return maxLength;
 }
 
+//Build a Factorial Calculator
+const num = 5;
+function factorialCalculator(num){
+  let result = 1;
+  for(let i = 1; i <= num; i++){
+    result = result * i;
+  }
+
+  return result;
+}
+
+const factorial = factorialCalculator(num)
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
