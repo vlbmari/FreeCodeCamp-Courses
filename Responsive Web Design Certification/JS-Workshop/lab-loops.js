b470a7e08858fd90b70e056f4a07b7a90e5a0f00
Mariana Lourenço <slb.marianavitoria@gmail.com>
2026-04-08 10:07:48 -0300

    JS - Loops Lab

--- Responsive Web Design Certification/JS-Workshop/lab-loops.js
+++ Responsive Web Design Certification/JS-Workshop/lab-loops.js
@@ -1,6 +1,6 @@
 //Build a Longest Word Finder App
 const findLongestWordLength = (sentence) => {
-  const arrSentence = sentence.trim().split(" ")
+  const arrSentence = sentence.trim().split(" ");
   let maxLength = 0;
   for(const word of arrSentence){
     if(word.length > maxLength){
@@ -33,10 +33,30 @@ function mutation(arr){
   const second = arr[1].toLowerCase();
   for(let i = 0; i < second.length; i++){
     if(!first.includes(second[i])){
-      return false
+      return false;
     }
   }
   return true;
 }
   
-console.log(mutation(names))
+console.log(mutation(names));
+
+//Implement the Chunky Monkey Algorithm
+const array = [0, 1, 2, 3, 4, 5];
+function chunkArrayInGroups(arr, size){
+  if (size < 1) {
+    console.log("Chunk size must be >= 1");
+    return;
+  }
+  let chunck = [];
+  for(let i = 0; i < arr.length; i += size){
+    chunck.push(arr.slice(i, i + size));
+  }
+  return chunck;
+  
+  
+}
+console.log(chunkArrayInGroups(array,2))
+
+
+
