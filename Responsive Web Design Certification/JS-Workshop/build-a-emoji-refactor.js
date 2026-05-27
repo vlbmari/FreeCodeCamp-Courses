0092763f5fadc332b45280b92eea29b07d90c3c0
SLB Mari <slb.marianavitoria@gmail.com>
2026-05-27 13:06:12 -0300

    JS - DOM Manipulation and Events - Workshop

---
+++ Responsive Web Design Certification/JS-Workshop/build-a-emoji-refactor.js
@@ -0,0 +1,15 @@
+function updateCount(btn) {
+  const countEl = btn.querySelector(".count");
+  let currCount = +countEl.textContent.split("/")[0];
+  
+  if (currCount === 10) return;
+  
+  currCount++;
+  
+  countEl.textContent = `${currCount}/10`;
+}
+
+const btns = document.querySelectorAll(".emoji-btn");
+btns.forEach(function(btn) {
+  btn.addEventListener("click", () => updateCount(btn))
+})
