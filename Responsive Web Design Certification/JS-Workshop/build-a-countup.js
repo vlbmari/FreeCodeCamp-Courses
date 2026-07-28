ca03873cfde986cc5e7911b7d2b5f91e894b3430
SLB Mari <slb.marianavitoria@gmail.com>
2026-07-28 15:48:23 -0300

    JS - Recursion - Workshop

---
+++ Responsive Web Design Certification/JS-Workshop/build-a-countup.js
@@ -0,0 +1,12 @@
+function countup(number) {
+  let countArray = [];
+  if (number < 1) {
+    return [];
+  } else {
+    countArray = countup(number - 1);
+    countArray.push(number);
+    return countArray;
+  }
+}
+
+console.log(countup(5))
