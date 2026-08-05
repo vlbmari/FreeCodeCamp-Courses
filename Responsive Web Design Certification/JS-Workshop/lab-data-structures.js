43d7e6a56c0fdb184d99399314a7596545442fc2
SLB Mari <slb.marianavitoria@gmail.com>
2026-08-05 16:43:08 -0300

    JS - Data Structures - Lab 3

--- Responsive Web Design Certification/JS-Workshop/lab-data-structures.js
+++ Responsive Web Design Certification/JS-Workshop/lab-data-structures.js
@@ -183,3 +183,34 @@ function clear(pilha){
 
 }
 
+//Implement a Queue
+function initQueue() {
+  return {
+    collection: []
+  };
+}
+
+function print(queue) {
+  console.log(queue.collection);
+}
+
+function enqueue(queue, element) {
+  queue.collection.push(element)
+}
+
+function dequeue(queue) {
+  return queue.collection.shift()
+}
+
+function front(queue) {
+  return queue.collection[0];
+}
+
+function size(queue) {
+  return queue.collection.length;
+}
+
+function isEmpty(queue) {
+  return queue.collection.length === 0 ? true : false;
+}
+
